8c4f5bfdc73a8bfd8466f2db90e163114a804003
Apurba <93421564+apurbatg@users.noreply.github.com>
2021-12-03 15:09:20 +0530

    Update admin.py

--- TGNRobot/modules/admin.py
+++ TGNRobot/modules/admin.py
@@ -761,6 +761,7 @@ UNPIN_HANDLER = CommandHandler("unpin", unpin, filters=Filters.group)
 INVITE_HANDLER = DisableAbleCommandHandler("invitelink", invite)
 
 PROMOTE_HANDLER = DisableAbleCommandHandler("promote", promote)
+FULLPROMOTE_HANDLER = DisableAbleCommandHandler("fullpromote", fullpromote)
 DEMOTE_HANDLER = DisableAbleCommandHandler("demote", demote)
 
 SET_TITLE_HANDLER = CommandHandler("title", set_title)
