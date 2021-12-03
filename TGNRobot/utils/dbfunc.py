b22359dcfe8106e1e9c4842cd837c8d88e603ceb
Apurba <93421564+apurbatg@users.noreply.github.com>
2021-12-03 13:18:11 +0530

    Update dbfunc.py

--- TGNRobot/utils/dbfunc.py
+++ TGNRobot/utils/dbfunc.py
@@ -3,7 +3,6 @@ from typing import Dict, List, Union
 
 
 coupledb = db.couple
-nsfwdb = db.nsfw
 
 
 # Couple Chooser
@@ -36,34 +35,4 @@ async def save_couple(chat_id: int, date: str, couple: dict):
             }
         },
         upsert=True
-    
-
-    
-
-async def is_nsfw_on(chat_id: int) -> bool:
-    chat = nsfwdb.find_one({"chat_id": chat_id})
-    if not chat:
-        return True
-    return False
-
-async def nsfw_on(chat_id: int):
-    is_nsfw = is_nsfw_on(chat_id)
-    if is_nsfw:
-        return
-    return nsfwdb.delete_one({"chat_id": chat_id})
-
-
-async def nsfw_off(chat_id: int):
-    is_nsfw = is_nsfw_on(chat_id)
-    if not is_nsfw:
-        return
-    return nsfwdb.insert_one({"chat_id": chat_id})
-
-
-async def is_served_chat(chat_id: int) -> bool:
-    chat = await chatsdb.find_one({"chat_id": chat_id})
-    if not chat:
-        return False
-    return True
- )
-        )
+    )
